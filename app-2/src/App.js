873fca56ccf7857c232a80af1a792c56c8fcc2f6
James Sheehan <jmsdevx@gmail.com>
2018-09-28 16:40:41 -0500

    completed react drills 1-3

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      array: ['hey','this','is', 'searching']
+    }
+  }
   render() {
+
+    let displayArray = this.state.array.map((element,index)=>{
+      return (<div className="map" key={index}>
+
+        <h2 >{element}</h2>
+
+      </div>
+        )
+    })
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>App 2</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div>{displayArray}</div>
+        
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      array: ['hey', 'this', 'is', 'searching'],
+      input: "",
+      finalArray: []
+    }
+  }
+
+  inputHandler(input, val){
+    this.setState({[input]:val})
+    console.log(this.state.input)
+  }
+
   render() {
+
+    let displayArray = this.state.array.filter((c) =>
+     c.includes(this.state.input))
+    .map((element, index)=>{
+      return(<div key={index}>
+        <h2>{element}</h2>
+        </div>)
+    })
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Search App 3</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" placeholder="search" value={this.state.input} onChange={(event)=>this.inputHandler("input",event.target.value)} /> 
+        {displayArray}
+
       </div>
     );
   }
