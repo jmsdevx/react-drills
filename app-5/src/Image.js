ee02abdcfff1f3b58adb51c8a352a18f4cd75f11
James Sheehan <jmsdevx@gmail.com>
2018-09-29 15:50:37 -0500

    fixed drill 5 to use functional component

---
+++ app-5/src/Image.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+function Image(props){
+
+    return(
+    <div>
+    <img src={props.image} alt="photo" />
+    </div>
+    )
+}
+
+
+
+
+
+
+
+export default Image
